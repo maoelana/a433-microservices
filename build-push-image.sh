55a140763f34a4c096db192d9ec7cd6a62b61ccf
Maulana <maulana@localhost.localdomain>
2025-05-07 10:16:33 +0700

    Add Dockerfile and upload image

---
+++ build-push-image.sh
@@ -0,0 +1,9 @@
+# Membuat image dari code project
+docker build -t ghcr.io/maoelana/a433-microservices/shipping-service:latest .
+
+# Docker login ke GitHub Container Registry
+# Sebelumnya atur dulu PASSWORD_GITHUB_REGISTRY sebagai environment variable dengan personal token
+echo $PASSWORD_GITHUB_REGISTRY | docker login ghcr.io -u maoelana --password-stdin
+
+# Push image ke GitHub Container Registry
+docker push ghcr.io/maoelana/a433-microservices/shipping-service:latest
